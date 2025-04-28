5294c40134fafb116875ffdf86da64dd45a7c197
Dos-Gringos <noah.webster.2000@gmail.com>
2025-04-28 14:01:56 -0600

    fixing installer

--- Installer/turtleInstaller.lua
+++ Installer/turtleInstaller.lua
@@ -6,11 +6,11 @@ if fs.exists("pathfinding.lua") then fs.delete("pathfinding.lua") end
 fs.makeDir("nav")
 
 -- download nav files
-shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_fuel.lua nav/fuel.lua")
-shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_move.lua nav/move.lua")
-shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_state.lua nav/state.lua")
-shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/init.lua nav/init.lua")
-shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/refs/heads/main/turtle_fleet/nav/detect_direction.lua nav/detect_direction.lua")
+shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_fuel.lua nav/fuel")
+shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_move.lua nav/move")
+shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/nav_state.lua nav/state")
+shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/main/turtle_fleet/nav/init.lua nav/init")
+shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet/refs/heads/main/turtle_fleet/nav/detect_direction.lua nav/detect_direction")
 
 -- download pathfinding
 shell.run("wget https://raw.githubusercontent.com/Dos-Gringos/TurtleTruckerFleet//main/turtle_fleet/pathfinding.lua pathfinding")
